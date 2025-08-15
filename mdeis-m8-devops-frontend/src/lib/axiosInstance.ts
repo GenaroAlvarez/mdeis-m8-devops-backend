2637261670c3d33cc16be4721b15e7ad910e083f
Genaro Alvarez <naroalvarez97@gmail.com>
2025-08-15 09:38:00 -0400

    chore: added httpsAgent: false on axios instance

--- mdeis-m8-devops-frontend/src/lib/axiosInstance.ts
+++ mdeis-m8-devops-frontend/src/lib/axiosInstance.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://localhost:5000/api/v1", // replace with your real API URL
+  httpsAgent: false,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
