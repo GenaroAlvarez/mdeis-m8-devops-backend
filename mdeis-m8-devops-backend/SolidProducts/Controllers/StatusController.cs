b3349bb43c532ef387dd18fadd0b1c6b5490a88a
Genaro Alvarez <naroalvarez97@gmail.com>
2025-08-28 22:21:42 -0400

    Added status endpoint for env check

---
+++ mdeis-m8-devops-backend/SolidProducts/Controllers/StatusController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SolidProducts.DTOs;
+
+namespace SolidProducts.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ClientResponseDto>>> GetStatus()
+        {
+            return Ok(new { Environment = "PROD", Status = "OK" });
+        }
+    }
+}
